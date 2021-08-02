f773bf3f7edac8c77ae663b1243038684546651b
jinhang <jinhang@newsinpalm.com>
2021-08-02 18:52:35 +0800

    change service

--- app/service/contact.ts
+++ app/service/contact.ts
@@ -1,19 +1,17 @@
 import { Service } from "egg";
 const cherrio = require('cheerio')
-import { contact } from '../../mock/index'
+import contactStr from '../../mock/contact'
 
 
 export default class ContactServce extends Service {
   async list ():Promise<Array<any>> {
-    const $ = cherrio.load(contact)
+    const $ = cherrio.load(contactStr)
     const _s = $('td')
     const res:Array<Array<any>> = []
-    Object.values(_s).map((val:any) => {
-      if (!val.children || !val.children[0]) return false
-      if (!val.children[0].children) return val.children[0].data
-      return val.children[0]?.children[0]?.data
-    }).forEach((val:any, index) => {
-      if (index % 5 === 0) {
+    Object.values(_s).filter((val:any) => val.data || val.data === '').map((val:any) => {
+      return val.data
+    }).forEach((val:string, index) => {
+      if (index % 6 === 0) {
         res.push([val])
       } else {
         res[res.length - 1].push(val)
@@ -22,9 +20,11 @@ export default class ContactServce extends Service {
     const data:Array<any> = res.filter((_val:any, index:number) => index !== 0).map((val:Array<string>) => {
       return {
         date: val[0],
-        append_from: val[1],
-        send_to: val[2],
-        theme: val[3],
+        append_from: val[2],
+        send_to: val[3],
+        theme: val[1],
+        sign: val[4],
+        type: val[5]
       }
     })
     return data.splice(0, data.length - 1)
